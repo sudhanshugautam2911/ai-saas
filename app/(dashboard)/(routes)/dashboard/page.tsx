26594d20ec1a1f30914797887298316479bf7fc9
sudhanshugautam2911 <gautamsudhanshu2911@gmail.com>
2023-12-26 20:19:54 +0530

    Part 1 - Sidebar mobile+laptop

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
 
 const dashboardPage = () => {
   return (
     <div>
-      <p className='text-6xl text-green-600'>Dashboard (protected)</p>
-      <UserButton afterSignOutUrl="/"/>
+      <p className='text-5xl text-green-600'>Dashboard (protected)</p>
     </div>
 
   )
